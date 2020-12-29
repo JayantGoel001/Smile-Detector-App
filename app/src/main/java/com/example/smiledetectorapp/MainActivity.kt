96a170acbad158bd1f074d55fca5de62dbda8a14
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-30 00:03:01 +0530

    Initial Commit

--- app/src/main/java/com/example/smiledetectorapp/MainActivity.kt
+++ app/src/main/java/com/example/smiledetectorapp/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.smiledetectorapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cameraButton: Button
+    private val REQUEST_IMAGE_CAPTURE = 123
+    private lateinit var image:InputImage
+    private lateinit var detector:FaceDetector
+
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        cameraButton = findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener {
+            val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePicture.resolveActivity(packageManager)!=null) {
+                startActivityForResult(takePicture,REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode==Activity.RESULT_OK){
+            val bitmap = data?.extras?.get("data") as Bitmap
+            detectFace(bitmap)
+        }
+    }
+
+    private fun detectFace(bitmap: Bitmap) {
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setClassificationMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setMinFaceSize(0.15f)
+            .enableTracking()
+            .build()
+
+        try {
+            image = InputImage.fromBitmap(bitmap)
+            detector = FaceDetection.getClient(options)
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        detector.process(image).addOnSuccessListener {
+
+        }
     }
 }
